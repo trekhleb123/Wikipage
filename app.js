2e7db7cdf7daedb1874d6292913ed11c1ea85ff7
trekhleb123 <trekhleb.al@gmail.com>
2020-03-12 12:21:38 -0500

    added tables and columns

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
-const path = require('path');
+const express = require("express")
+const app = express()
+const morgan = require("morgan")
+const path = require("path")
+const { db, Page, User } = require("./models")
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: false }));
+app.use(morgan("dev"))
+app.use(express.static(path.join(__dirname, "public")))
+app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res, next) => {
-	res.send('hello world');
-});
+app.get("/", (req, res, next) => {
+  res.send("hello world")
+})
 
-app.listen(3000, () => {
-	console.log(`listening on port: 3000`);
-});
+db.authenticate().then(() => {
+  console.log("connected to the database")
+})
+
+async function page() {
+  try {
+    await db.sync({ force: true })
+    app.listen(3000, () => {
+      console.log(`listening on port: 3000`)
+    })
+  } catch (err) {
+    console.log(err)
+    await db.close()
+  }
+}
+page()
